bd1c8742eabec57c744f39b79dcd582f5a1f1747
Matthias Hulme <mh828@byu.edu>
2023-03-01 22:48:03 -0700

    Add project files.

---
+++ Mission09_mh828/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_mh828.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_mh828.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookRepository repo;
+
+        public HomeController (IBookRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index()
+        {
+            var returnList = repo.Books.ToList();
+
+            return View(returnList);
+        }
+    }
+}
---
+++ Mission09_mh828/Models/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_mh828.Models
+{
+    public class IBookRepository
+    {
+        public interface IBookRepository
+        {
+            IQueryable<Book> Books { get; }
+        }
+    }
+}
